c62f50853f2503891039287e1466e3bb334d76f8
rakibDG <rakibul.hasan@technonext.com>
2024-02-04 17:55:54 +0600

    Working on Access token and refresh token

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import {activateUser, loginUser, logoutUser, registrationUser} from "../controllers/user.controller";
+import {activateUser, loginUser, logoutUser, registrationUser, updateAccessToken} from "../controllers/user.controller";
+import {authorizeRoles, isAuthenticated} from "../middleware/auth";
 const userRouter=express.Router();
 userRouter.post('/registration',registrationUser)
 userRouter.post('/activate-user',activateUser)
 userRouter.post('/login',loginUser)
-userRouter.post('/logout',logoutUser )
+userRouter.get('/logout',isAuthenticated,logoutUser)
+userRouter.get("/refresh",updateAccessToken)
 export default userRouter;
